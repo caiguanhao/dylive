1a79eb8e817b7782440d56fc131031c5138b4672
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2023-09-05 16:26:36 +0800

    update

--- live.go
+++ live.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"net/url"
 	"strconv"
 	"strings"
 	"sync"
@@ -22,39 +21,19 @@ type (
 	}
 
 	dyliveCategories struct {
-		App struct {
-			LayoutData struct {
-				CategoryTab struct {
-					CategoryData []struct {
-						Partition struct {
-							IDStr string `json:"id_str"`
-							Type  int    `json:"type"`
-							Title string `json:"title"`
-						} `json:"partition"`
-					} `json:"categoryData"`
-				} `json:"categoryTab"`
-			} `json:"layoutData"`
-		} `json:"app"`
-		Four struct {
-			PartitionData struct {
-				Partition struct {
-					IDStr string `json:"id_str"`
-					Type  int    `json:"type"`
-					Title string `json:"title"`
-				} `json:"partition"`
-				SubPartition []struct {
-					IDStr string `json:"id_str"`
-					Type  int    `json:"type"`
-					Title string `json:"title"`
-				} `json:"sub_partition"`
-			} `json:"partitionData"`
-		} `json:"874cbd3ca82b27af9f285883fd26e52f"`
+		CategoryData []struct {
+			Partition struct {
+				IDStr string `json:"id_str"`
+				Type  int    `json:"type"`
+				Title string `json:"title"`
+			} `json:"partition"`
+		} `json:"categoryData"`
 	}
 )
 
 // GetCategories gets all Douyin live stream categories.
 func GetCategories(ctx context.Context) ([]Category, error) {
-	const first = "1_620"
+	const first = "1_4609"
 	var cats []Category
 	var subCats []Category
 	err := getCategories(ctx, first, &cats, &subCats)
@@ -83,17 +62,21 @@ func getCategories(ctx context.Context, id string, categories, subCategories *[]
 	if categories == nil && subCategories == nil {
 		return nil
 	}
-	rd, err := getCategoryPageRenderData(ctx, id)
+	data, err := getCategoryPageData(ctx, id, "categoryData", "partitionData")
 	if err != nil {
 		return err
 	}
+	categoryData, partitionData := data[0], data[1]
 	var cats dyliveCategories
-	err = json.Unmarshal([]byte(rd), &cats)
-	if err != nil {
+	if err := getDataInArray(categoryData, &cats); err != nil {
+		return err
+	}
+	var cat dyliveCategory
+	if err := getDataInArray(partitionData, &cat); err != nil {
 		return err
 	}
 	if categories != nil {
-		for _, cat := range cats.App.LayoutData.CategoryTab.CategoryData {
+		for _, cat := range cats.CategoryData {
 			*categories = append(*categories, Category{
 				Id:   fmt.Sprintf("%d_%s", cat.Partition.Type, cat.Partition.IDStr),
 				Name: cat.Partition.Title,
@@ -101,8 +84,8 @@ func getCategories(ctx context.Context, id string, categories, subCategories *[]
 		}
 	}
 	if subCategories != nil {
-		p := cats.Four.PartitionData.Partition
-		for _, cat := range cats.Four.PartitionData.SubPartition {
+		p := cat.PartitionData.Partition
+		for _, cat := range cat.PartitionData.SubPartition {
 			*subCategories = append(*subCategories, Category{
 				Id:   fmt.Sprintf("%d_%s_%d_%s", p.Type, p.IDStr, cat.Type, cat.IDStr),
 				Name: cat.Title,
@@ -132,63 +115,67 @@ type (
 	}
 
 	dyliveCategory struct {
-		Four struct {
-			RoomsData struct {
-				Data []struct {
-					Room struct {
-						Title string `json:"title"`
-						Stats struct {
-							TotalUserStr string `json:"total_user_str"`
-							UserCountStr string `json:"user_count_str"`
-						} `json:"stats"`
-						Owner struct {
-							Nickname string `json:"nickname"`
-						} `json:"owner"`
-						StreamUrl struct {
-							FlvPullUrl    map[string]string `json:"flv_pull_url"`
-							HlsPullUrlMap map[string]string `json:"hls_pull_url_map"`
-						} `json:"stream_url"`
-						RoomViewStats struct {
-							DisplayValue int `json:"display_value"`
-						} `json:"room_view_stats"`
-					} `json:"room"`
-					WebRid    string `json:"web_rid"`
-					StreamSrc string `json:"streamSrc"`
-					Cover     string `json:"cover"`
-					Avatar    string `json:"avatar"`
-				} `json:"data"`
-			} `json:"roomsData"`
-			PartitionData struct {
-				Partition struct {
-					IDStr string `json:"id_str"`
-					Type  int    `json:"type"`
+		RoomsData struct {
+			Data []struct {
+				Room struct {
 					Title string `json:"title"`
-				} `json:"partition"`
-				SelectPartition struct {
-					IDStr string `json:"id_str"`
-					Type  int    `json:"type"`
-					Title string `json:"title"`
-				} `json:"select_partition"`
-			} `json:"partitionData"`
-		} `json:"874cbd3ca82b27af9f285883fd26e52f"`
+					Stats struct {
+						TotalUserStr string `json:"total_user_str"`
+						UserCountStr string `json:"user_count_str"`
+					} `json:"stats"`
+					Owner struct {
+						Nickname string `json:"nickname"`
+					} `json:"owner"`
+					StreamUrl struct {
+						FlvPullUrl    map[string]string `json:"flv_pull_url"`
+						HlsPullUrlMap map[string]string `json:"hls_pull_url_map"`
+					} `json:"stream_url"`
+					RoomViewStats struct {
+						DisplayValue int `json:"display_value"`
+					} `json:"room_view_stats"`
+				} `json:"room"`
+				WebRid    string `json:"web_rid"`
+				StreamSrc string `json:"streamSrc"`
+				Cover     string `json:"cover"`
+				Avatar    string `json:"avatar"`
+			} `json:"data"`
+		} `json:"roomsData"`
+		PartitionData struct {
+			Partition struct {
+				IDStr string `json:"id_str"`
+				Type  int    `json:"type"`
+				Title string `json:"title"`
+			} `json:"partition"`
+			SelectPartition struct {
+				IDStr string `json:"id_str"`
+				Type  int    `json:"type"`
+				Title string `json:"title"`
+			} `json:"select_partition"`
+			SubPartition []struct {
+				IDStr string `json:"id_str"`
+				Type  int    `json:"type"`
+				Title string `json:"title"`
+			} `json:"sub_partition"`
+		} `json:"partitionData"`
 	}
 )
 
 // GetRoomsByCategory gets top 15 Douyin live stream rooms of a category.
 func GetRoomsByCategory(ctx context.Context, categoryId string) ([]Room, error) {
-	rd, err := getCategoryPageRenderData(ctx, categoryId)
+	data, err := getCategoryPageData(ctx, categoryId, "roomsData")
 	if err != nil {
 		return nil, err
 	}
+	roomsData := data[0]
 	var cat dyliveCategory
-	err = json.Unmarshal([]byte(rd), &cat)
-	if err != nil {
+	if err := getDataInArray(roomsData, &cat); err != nil {
 		return nil, err
 	}
+
 	var rooms []Room
-	for _, room := range cat.Four.RoomsData.Data {
-		p := cat.Four.PartitionData.Partition
-		c := cat.Four.PartitionData.SelectPartition
+	for _, room := range cat.RoomsData.Data {
+		p := cat.PartitionData.Partition
+		c := cat.PartitionData.SelectPartition
 		var count string
 		if room.Room.RoomViewStats.DisplayValue > 0 {
 			count = strconv.Itoa(room.Room.RoomViewStats.DisplayValue)
@@ -223,44 +210,94 @@ func GetRoomsByCategory(ctx context.Context, categoryId string) ([]Room, error) 
 	return rooms, nil
 }
 
-func getCategoryPageRenderData(ctx context.Context, id string) (string, error) {
+func getCategoryPageData(ctx context.Context, id string, filters ...string) ([]string, error) {
 	req, err := http.NewRequestWithContext(ctx, "GET", "https://live.douyin.com/category/"+id, nil)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	req.Header.Set("User-Agent", userAgent)
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	defer resp.Body.Close()
 	b, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	return getRenderData(string(b)), nil
+	parts := getDataInHtml(string(b))
+	var output []string
+	for _, filter := range filters {
+		var ret string
+		for _, part := range parts {
+			if strings.Contains(part, filter) {
+				ret = part
+				break
+			}
+		}
+		output = append(output, ret)
+	}
+	return output, nil
 }
 
-func getRenderData(input string) (output string) {
-	a := strings.Index(input, "RENDER_DATA")
-	if a < 0 {
-		return
+func getDataInHtml(input string) (output []string) {
+	const funcName = "__pace_f"
+	const endTag = "</script>"
+	var parts []string
+	for {
+		a := strings.Index(input, funcName)
+		if a == -1 {
+			break
+		}
+		input = input[a+len(funcName):]
+		b := strings.Index(input, `"`)
+		if b < 0 {
+			continue
+		}
+		input = input[b+1:]
+		b = strings.Index(input, endTag)
+		if b < 0 {
+			continue
+		}
+		b = strings.LastIndex(input[:b], `"`)
+		if b < 0 {
+			continue
+		}
+		var ret string
+		if json.Unmarshal([]byte(`"`+input[:b]+`"`), &ret) != nil {
+			continue
+		}
+		parts = append(parts, ret)
 	}
-	input = input[a:]
-	a = strings.Index(input, ">")
-	if a < 0 {
-		return
+	parts = strings.Split(strings.Join(parts, "\n"), "\n")
+	for _, part := range parts {
+		a := strings.IndexAny(part, "[{")
+		if a == -1 {
+			continue
+		}
+		b := strings.LastIndexAny(part, "}]")
+		if b == -1 {
+			continue
+		}
+		output = append(output, part[a:b+1])
 	}
-	input = input[a+1:]
-	a = strings.Index(input, "<")
-	if a < 0 {
-		return
-	}
-	input = input[:a]
-	input, err := url.QueryUnescape(input)
-	if err != nil {
-		return
-	}
-	output = input
 	return
+}
+
+func getDataInArray(input string, target interface{}) error {
+	var array []interface{}
+	if err := json.Unmarshal([]byte(input), &array); err != nil {
+		return err
+	}
+	for _, element := range array {
+		switch v := element.(type) {
+		case map[string]interface{}:
+			jsonStr, err := json.Marshal(v)
+			if err != nil {
+				continue
+			}
+			return json.Unmarshal(jsonStr, target)
+		}
+	}
+	return nil
 }
